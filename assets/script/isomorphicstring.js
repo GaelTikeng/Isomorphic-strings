fdd10138cb3379a14396c05a94c73f68c92e053d
GaelTikeng <Gaelinho@yahoo.fr>
2023-08-25 10:52:23 +0100

    correcting linters errors

--- assets/script/isomorphicstring.js
+++ assets/script/isomorphicstring.js
@@ -15,15 +15,15 @@ function isomorphicString () {
   const s = document.querySelector('.string1').value
   const t = document.querySelector('.string2').value
 
-  if (s.length !== t.length) return displayResult.innerHTML = 'The two strings must bee the same';
+  if (s.length !== t.length) return displayResult.innerHTML = 'The two strings must bee the same'
 
-  const mapa = new Map();
-  const mapb = new Map();
+  const mapa = new Map()
+  const mapb = new Map()
 
   for (let i = 0; i < s.length; i++) {
     if (mapa.has(s[i])) {
       if (mapa.get(s[i]) !== t[i]) {
-        return displayResult.innerHTML = 'false';
+        return displayResult.innerHTML = 'false'
       }
     } else {
       mapa.set(s[i], t[i])
@@ -31,7 +31,7 @@ function isomorphicString () {
 
     if (mapb.has(t[i])) {
       if (mapb.get(t[i]) !== s[i]) {
-        return displayResult.innerHTML = 'false';
+        return displayResult.innerHTML = 'false'
       }
     } else {
       mapb.set(t[i], s[i])
